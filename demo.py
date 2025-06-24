f5d62a00734ba536dfeab1ae7eb2a5c62cf9bec8
lunbe <judy47197@gmail.com>
2025-06-24 14:42:42 +0800

    第一版本：增加搜尋dbuser

--- demo.py
+++ demo.py
@@ -1,2 +1,13 @@
 import mysql.connector
 
+print("建立資料庫連線中")
+mysql.connector.connect(
+
+    host="127.0.0.1",
+    port=3306,
+    user="dbuser",
+    password="1234"
+
+)
+
+print("透過連線取得cursor物件")
