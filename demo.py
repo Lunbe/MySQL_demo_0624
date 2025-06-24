5e9e67ce291754d13d9464189cf7f53c3c09e3bc
lunbe <judy47197@gmail.com>
2025-06-24 15:25:05 +0800

    第二版本：增加world資料庫

--- demo.py
+++ demo.py
@@ -1,13 +1,21 @@
+#from flask import ctx
 import mysql.connector
 
 print("建立資料庫連線中")
-mysql.connector.connect(
+cnx=mysql.connector.connect(
 
     host="127.0.0.1",
     port=3306,
     user="dbuser",
-    password="1234"
-
+    password="1234",
+    database="world"
 )
 
 print("透過連線取得cursor物件")
+
+dbcursor=cnx.cursor()
+print("執行select name form city") #否則就要 select * from database.table
+print("記得開權限") #就是在MYSQLworkbench中跑程式的內容
+
+dbcursor.execute("select name from world.city")
+
